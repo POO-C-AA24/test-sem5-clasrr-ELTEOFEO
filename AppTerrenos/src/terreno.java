7c932f54bcbbc0864a6bb64e3b2a6608869c6698
Manuel <machavez35@utpl.edu.ec>
2024-05-08 16:27:11 -0500

    holi

---
+++ AppTerrenos/src/terreno.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author COSAS DE LA Y
+ */
+public class terreno {
+    
+}
